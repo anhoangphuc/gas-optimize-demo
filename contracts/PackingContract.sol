fc97e15829af8d7600a6bc34921e6fb6b4032026
hoangphuc <hoangphucnb97@gmail.com>
2022-09-28 13:02:07 +0700

    Add Packing contract and demo code in address

---
+++ contracts/PackingContract.sol
@@ -0,0 +1,39 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity 0.8.12;
+
+contract PackingContract {
+    uint112 reserve0;
+    uint112 reserve1;
+    uint32 blockTimestamp;
+    uint256 total;
+
+    constructor() {}
+
+    function getValue() external view returns (uint112 reserve0_, uint112 reserve1_, uint32 blockTimestamp_) {
+        reserve0_ = reserve0;    
+        reserve1_ = reserve1;
+        blockTimestamp_ = blockTimestamp;
+    }
+
+    function setValue(uint112 reverse0_, uint112 reverse1_, uint32 blockTimestamp_) external {
+        reserve0 = reverse0_;
+        reserve1 = reverse1_;
+        blockTimestamp = blockTimestamp_;
+    }
+
+    function getTotal() external view returns (uint256 total_) {
+        total_ = total;
+    }
+
+    function setTotal(uint256 total_) external {
+        total = total_;
+    }
+
+    function getReserve0() external view returns (uint112 reserve0_) {
+        reserve0_ = reserve0;
+    }
+
+    function setReserve0(uint112 reserver0_) external {
+        reserve0 = reserver0_;
+    }
+}
---
+++ test/simulate/1_get_contract_code.ts
@@ -0,0 +1,17 @@
+import { network, ethers } from "hardhat"
+
+(async function main() {
+    const [account1] = await ethers.getSigners();
+    const PackingContract = await ethers.getContractFactory('PackingContract');
+    const packingContract = await PackingContract.deploy();
+    await packingContract.deployed();
+    console.log(`Contract Packing is deployed at address ${packingContract.address}`);
+    console.log('--------------------');
+    console.log(`Code deployed at address ${packingContract.address} is:`)
+    const contractCode = await network.provider.send("eth_getCode", [packingContract.address]);
+    console.log(contractCode);
+    console.log('--------------------');
+    const accountCode = await network.provider.send("eth_getCode", [account1.address])
+    console.log(`Code of account ${account1.address} is:`) 
+    console.log(accountCode);
+})()
