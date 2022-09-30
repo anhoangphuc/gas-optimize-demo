a46bf665daa7418b28a7ca722d5e94c834c4eaf2
hoangphuc <hoangphucnb97@gmail.com>
2022-09-30 09:35:23 +0700

    Change StorageOperation contract layout

---
+++ contracts/StorageOperation.sol
@@ -0,0 +1,41 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity 0.8.12;
+
+contract StorageOperation {
+    uint256[] arr = [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+    uint256 sum;
+
+    function addValues(uint256[] calldata x) external {
+        for (uint256 i = 0; i < x.length; i++) {
+            arr.push(x[i]);
+        }
+    }
+
+    function getValue() external view returns (uint256[] memory) {
+        return arr;
+    }
+
+    function calculateSum() external {
+        sum = 0;
+        for (uint256 i = 0; i < arr.length; i++) {
+            sum = sum + arr[i];
+        }
+    }
+
+    function betterCalculateSum() external {
+        uint256 localSum = 0;
+        for (uint256 i = 0; i < arr.length; i++) {
+            localSum = localSum + arr[i];
+        }
+        sum = localSum;
+    }
+
+    function bestCalculateSum() external {
+        uint256 localSum = 0;
+        uint256 len = arr.length;
+        for (uint256 i = 0; i < len; i++) {
+            localSum = localSum + arr[i];
+        }
+        sum = localSum;
+    }
+}
---
+++ test/StorageOperation.ts
@@ -0,0 +1,23 @@
+import { ethers } from "hardhat";
+import { StorageOperation } from "../typechain-types"
+
+context(`StorageOperation`, async () => {
+    let storageOperation: StorageOperation;
+    beforeEach(async () => {
+        const StorageOperation = await ethers.getContractFactory('StorageOperation');
+        storageOperation = await StorageOperation.deploy();
+        await storageOperation.deployed();
+    })
+
+    it(`calculateSum`, async () => {
+        await storageOperation.calculateSum();
+    })
+
+    it(`betterCalculateSum`, async () => {
+        await storageOperation.betterCalculateSum();
+    })
+
+    it(`bestCalculateSum`, async () => {
+        await storageOperation.bestCalculateSum();
+    })
+})
